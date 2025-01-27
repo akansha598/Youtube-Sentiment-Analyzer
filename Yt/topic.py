8e9bac87cf4dc8ca3c84113080d68719860f890e
akansha598 <159223025+akansha598@users.noreply.github.com>
2025-01-27 20:56:50 +0530

    Update topic.py

--- Yt/topic.py
+++ Yt/topic.py
@@ -103,22 +103,35 @@ def extract_topics_for_next_video(comments, num_topics=10):
     
     # Define suggestion trigger phrases, including subscriber-specific phrases
     suggestion_phrases = [
-        "can you make", "you should make", "i want", "next video", 
-        "it would be great if", "please create", "i'd love to see", 
-        "could you do", "how about", "make a video on", 
-        "please make", "can you do", "you should cover", 
-        "it would be nice if", "a tutorial on", "you should try", 
-        "a video about", "i would love to see", "can you explain", 
-        "could you talk about", "could you review", "please review", 
-        "can you recommend", "you should recommend", "can you discuss", 
-        "recommend books", "recommend movies", "book reviews", 
-        "movie reviews", "next book", "next movie", "book suggestions", 
-        "movie suggestions", "suggest a book", "suggest a movie",
-        "i recommend", "my favorite", "we want to see", "subscribers would love",
-        "the community suggests", "our choice is", "you should include", 
-        "please add", "check out", "consider reviewing", "talk about",
-        "can you feature", "feature this", "my choice is", "my suggestion is"
+    "can you make", "you should make", "i want", "next video", 
+    "it would be great if", "please create", "i'd love to see", 
+    "could you do", "how about", "make a video on", 
+    "please make", "can you do", "you should cover", 
+    "it would be nice if", "a tutorial on", "you should try", 
+    "a video about", "i would love to see", "can you explain", 
+    "could you talk about", "could you review", "please review", 
+     "we want to see", "subscribers would love",
+    "the community suggests", "our choice is", "you should include", 
+    "please add", "check out", "consider reviewing", "talk about",
+    "can you feature", "feature this", "my choice is", "my suggestion is",
+    "can you make a recipe for", "you should make a video on", "i want to see", 
+    "next video, could you do", "it would be great if you could share", 
+    "please create a video on", "i'd love to see", "can you do a video about", 
+    "how about doing a video on", "make a video on", "please make a video on", 
+    "can you do a recipe for", "you should cover", "it would be nice if you could show", 
+    "a tutorial on", "you should try making", "a video about", "i would love to see a recipe for", 
+    "can you explain how to make", "could you talk about making", "could you review", 
+    "please review a recipe for", "can you recommend a recipe for", "you should recommend some", 
+    "can you discuss recipes for", "recommend recipes for", "next recipe video, could you make", 
+    "next recipe video, how about trying", "recipe suggestions for", "suggest a recipe for", 
+    "i recommend trying", "my favorite recipe is", "we want to see more recipes for", 
+    "subscribers would love a video on", "the community suggests a recipe for", 
+    "our choice is a recipe for", "you should include a recipe for", "please add a recipe for", 
+    "check out this recipe idea", "consider reviewing a recipe for", "talk about making a recipe for", 
+    "can you feature a recipe for", "feature a recipe for", "my choice is a recipe for", 
+    "my suggestion is a recipe for"
     ]
+
 
     # Extract sentences containing suggestion phrases
     suggestion_sentences = []
